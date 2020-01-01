b4b585f51d13bc8f46151a21fe037cfa13030fd1
Bill Nicholson <nicholdw@ucmail.uc.edu>
2020-01-01 09:34:04 -0500

    First Commit

---
+++ DataAnalyticsStockHistory/CompanyFinancials.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ * https://datahub.io/core/s-and-p-500-companies-financials
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAnalyticsStockHistory
+{
+    class CompanyFinancials
+    {
+        public CompanyFinancials(String symbol, String name, String sector, double price, double priceEarnings) {
+            this.symbol = symbol;
+            this.name = name;
+            this.price = price;
+            this.priceEarnings = priceEarnings;
+        }
+
+        public CompanyFinancials(String symbol, String name, String sector, double price, double priceEarnings, 
+                                 double dividendYield, double earningsPerShare, 
+                                 double fiftyTwoWeekLow, double fiftyTwoWeekHigh, double marketCap,
+                                 double EBITDA, double priceSales,double priceBook, String secFiling) {
+            this.symbol = symbol;
+            this.name = name;
+            this.sector = sector;
+            this.price = price;
+            this.priceEarnings = priceEarnings;
+            this.dividendYield = dividendYield;
+            this.earningsPerShare = earningsPerShare;
+            this.fiftyTwoWeekHigh = fiftyTwoWeekHigh;
+            this.fiftyTwoWeekLow = fiftyTwoWeekLow;
+            this.marketCap = marketCap;
+            this.EBIDTA = EBIDTA;
+            this.priceSales = priceSales;
+            this.priceBook = priceBook;
+            this.secFiling = secFiling;
+        }
+        // Symbol,Name,Sector,Price,Price/Earnings,Dividend Yield,Earnings/Share,
+        //52 Week Low,52 Week High,Market Cap,EBITDA,Price/Sales,Price/Book,SEC Filings
+        public String symbol { get; set; }
+        public String name { get; set; }
+        public String sector { get; set; }
+        public double price { get; set; }
+        public double priceEarnings { get; set; }
+        public double dividendYield { get; set; }
+        public double earningsPerShare { get; set; }
+        public double fiftyTwoWeekLow { get; set; }
+        public double fiftyTwoWeekHigh { get; set; }
+        public double marketCap { get; set; }
+        public double EBIDTA { get; set; }
+        public double priceSales { get; set; }
+        public double priceBook { get; set; }
+        public String secFiling { get; set; }
+
+        public override String ToString() { return symbol + ": " + name; }
+    }
+}
--- DataAnalyticsStockHistory/Program.cs
+++ DataAnalyticsStockHistory/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ * https://datahub.io/core/s-and-p-500-companies-financials
+ */
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace DataAnalyticsStockHistory
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Process() + " records processed.");
+        }
+        public static int Process() {
+            Dictionary<String, CompanyFinancials> company = new Dictionary<string, CompanyFinancials>();
+            int totalProcessed = 0;
+            String[] data = File.ReadAllLines("..\\..\\data\\constituents-financials_csv.csv");
+            // Each element of data is a line in the CSV file. The first element is the column headers, not data.
+            int count = 0;
+            foreach (String s in data) {
+                if (count > 0) {
+                    //Console.WriteLine(s);
+                    String[] tmp = null;
+                    try {
+                        // ToDo: The split on comma doesn't work so well because there are commas in company names. 
+                        // Those company names are enclosed in quotes so additional processing is needed. For now, there's a try/catch that
+                        //  allows processing to continue and simply discards the companies that cannot be processed.
+                       tmp = s.Split(',');
+                        CompanyFinancials cf = new CompanyFinancials(tmp[0], tmp[1], tmp[2], Convert.ToDouble(tmp[3]), Convert.ToDouble(tmp[4]));
+                        company.Add(cf.symbol, cf);
+                        totalProcessed++;
+                    } catch (Exception ex) {
+                        try { Console.WriteLine("Could not add " + tmp[0]); } catch (Exception ex1){ }
+                    }
+                }
+                count++;
+            }
+            return totalProcessed;
         }
     }
 }
